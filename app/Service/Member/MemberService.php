ee73db8f562b65871c759a2c5b67ffde3e0c6d39
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2017-10-19 23:27:26 +0900

    add Service層生成ロジックを追加

---
+++ app/Service/Member/MemberService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service\Member;
+
+class MemberService
+{
+    public function storeMember()
+    {
+
+    }
+
+    public function updateMember()
+    {
+
+    }
+
+    public function showMember()
+    {
+
+    }
+
+    public function destroyMember()
+    {
+        
+    }
+}
